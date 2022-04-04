56f8f3114826a0e2c5c67761e5bd49645cac386c
SantiagoGarcia8 <sjgarciat@correo.udistrital.edu.co>
2022-04-03 22:06:34 -0500

    Se termina de agregar las clases tabla

---
+++ src/main/java/Tablas/EntrenadorPersonalizado.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tablas;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author sjgar
+ */
+public class EntrenadorPersonalizado extends Entrenador {
+    
+    private int num_cli;
+
+    public EntrenadorPersonalizado(int num_cli, String nombre, int cedula, String direccion, int telefono, String correo, int gym, Date fecha_nacimiento, char sexo, String contraseña, double sueldo, int id_cli) {
+        super(nombre, cedula, direccion, telefono, correo, gym, fecha_nacimiento, sexo, contraseña, sueldo, id_cli);
+        this.num_cli = num_cli;
+    }
+
+    public int getNum_cli() {
+        return num_cli;
+    }
+
+    public void setNum_cli(int num_cli) {
+        this.num_cli = num_cli;
+    }
+
+    @Override
+    public String toString() {
+        return "EntrenadorPersonalizado{" + "nombre= " + this.getNombre() + ", cedula= " + this.getCedula() + ", direccón= " + this.getDireccion() + ", telefono= " + this.getTelefono() + ", correo= " + this.getCorreo() +
+                ", gym= " + this.getGym() + ",fecha nacimiento= " + this.getFecha_nacimiento() + ", sexo= " + this.getSexo() + ", sueldo=" + this.getSueldo() + ", id_cli=" + this.getId_cli() + "num_cli=" + num_cli + '}';
+    }
+    
+    
+    
+}
