34f66c889ca7bfd2fcf1d9bc6ebcd252a33d745a
SantiagoGarcia8 <sjgarciat@correo.udistrital.edu.co>
2022-04-04 10:07:36 -0500

    Create SentenciasSQL.java

---
+++ src/main/java/Tablas/SentenciasSQL.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tablas;
+
+/**
+ *
+ * @author sjgar
+ */
+public interface SentenciasSQL {
+    
+    
+    /*
+    
+    SELECT c.nombre, c.id_plan, p.nombre FROM cliente AS c INNER JOIN plan AS p ON c.id_plan = p.id_plan;
+    SELECT e.nombre, e.id_gym FROM empleados AS e INNER JOIN gimnasio AS g ON e.id_gym = g.id_gym;
+    
+    */
+    
+}
